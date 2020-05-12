3cacd805d2e540abca62eb35eed45ad6e62fb96e
DinaraBulatova <32076999+DinaraBulatova@users.noreply.github.com>
2020-05-12 18:25:37 +0200

    Add files via upload

---
+++ Capstone assignment week 1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be used for the Capstone weekly assignments
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+import numpy as np
+
+
+# In[7]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
